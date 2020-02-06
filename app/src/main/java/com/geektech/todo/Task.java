019b0fb091f524ca6fe5e4a5be82e62298302d38
unknown <natlusrun94@gamil.com>
2020-02-06 18:32:20 +0300

    Initial commit

---
+++ app/src/main/java/com/geektech/todo/Task.java
@@ -0,0 +1,10 @@
+package com.geektech.todo;
+
+import java.io.Serializable;
+
+public class Task implements Serializable {
+    public String title;
+    public String description;
+    public String deadline; // TODO: Change to Date type
+
+}
---
+++ app/src/main/java/com/geektech/todo/TaskViewHolder.java
@@ -0,0 +1,8 @@
+package com.geektech.todo;
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
